59f74b6b1f857c5588099e92a90e272c620b8299
Caballou <leandro.damian93@gmail.com>
2023-01-31 15:37:18 -0300

    reduce image size

--- src/components/Puntaje.js
+++ src/components/Puntaje.js
@@ -4,7 +4,12 @@ import '../stylesheets/Puntaje.css'
 function Puntaje({ numPuntos }){
   return (
     <div className='puntos'>
-      <img className='fernet' src={`./anotador-de-truco/img/fernet-${numPuntos}.png`} alt='fernecitos'></img>
+      {
+      numPuntos != 0 ?
+      <img className='fernet' src={`/anotador-de-truco/img/fernet-${numPuntos}.png`} alt='fernecitos'></img>
+      :
+      ''
+      }
     </div>
   )
 }
